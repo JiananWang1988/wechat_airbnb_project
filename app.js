950fad6a4f1ada1f65fec10fad29531eaee3514e
Samir Bhojwani <sbhojwani1@gmail.com>
2018-11-21 13:45:54 +0800

    Added edit and add as well as changed index

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ App({
         "name": "Old Skool Nokia",
         "price": "150RMB",
         "description": "More phone, More Fun",
-        "image": "https://img.grouponcdn.com/deal/8DDtq5XRzVnLXEUnPHPd/p2-2048x1229/v1/c700x420.jpg",
+        "image": "/images/chargers/nokia.jpg",
         "avatar": "https://kitt.lewagon.com/placeholder/users/tgenaitay"
       },
       {
@@ -72,15 +72,15 @@ App({
         "name": "iPhone Charger",
         "price": "150RMB",
         "description": "Best Southeast Dishes",
-        "image": "https://assets.epicurious.com/photos/57a384e73a12dd9d5602415e/2:1/w_1260%2Ch_630/mango-salad.jpg",
+        "image": "/images/chargers/case.jpg",
         "avatar": "https://kitt.lewagon.com/placeholder/users/tgenaitay"
       },
       {
         "id": 4,
-        "name": "Perfect Charger  ",
-        "price": "42 american drive",
-        "description": "More Meat Than You Can Eat",
-        "image": "https://media-cdn.tripadvisor.com/media/photo-s/11/76/1c/72/stock-burger-co.jpg",
+        "name": "car Charger",
+        "price": "210RMB",
+        "description": "Charge in your Car!",
+        "image": "/images/chargers/car.jpg",
         "avatar": "https://kitt.lewagon.com/placeholder/users/tgenaitay"
       }
     ]
