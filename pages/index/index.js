27a70dccba5787dba20f72badb0e8ad5f1aeeece
Samir Bhojwani <sbhojwani1@gmail.com>
2018-11-21 10:24:25 +0800

    Slides to Complete Frontend

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,90 @@
 //index.js
 //获取应用实例
-const app = getApp()
+// const app = getApp()
+
+// Page({
+//   data: {
+//     motto: 'Samirs World',
+//     userInfo: {},
+//     hasUserInfo: false,
+//     canIUse: wx.canIUse('button.open-type.getUserInfo')
+//   },
+//   //事件处理函数
+//   bindViewTap: function() {
+//     wx.navigateTo({
+//       url: '../logs/logs'
+//     })
+//   },
+//   onLoad: function () {
+//     if (app.globalData.userInfo) {
+//       this.setData({
+//         userInfo: app.globalData.userInfo,
+//         hasUserInfo: true
+//       })
+//     } else if (this.data.canIUse){
+//       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+//       // 所以此处加入 callback 以防止这种情况
+//       app.userInfoReadyCallback = res => {
+//         this.setData({
+//           userInfo: res.userInfo,
+//           hasUserInfo: true
+//         })
+//       }
+//     } else {
+//       // 在没有 open-type=getUserInfo 版本的兼容处理
+//       wx.getUserInfo({
+//         success: res => {
+//           app.globalData.userInfo = res.userInfo
+//           this.setData({
+//             userInfo: res.userInfo,
+//             hasUserInfo: true
+//           })
+//         }
+//       })
+//     }
+//   },
+//   getUserInfo: function(e) {
+//     console.log(e)
+//     app.globalData.userInfo = e.detail.userInfo
+//     this.setData({
+//       userInfo: e.detail.userInfo,
+//       hasUserInfo: true
+//     })
+//   }
+// })
+
+// ========================================================
+
+//index.js
+var app = getApp()
+// This is the access to the app.js file
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+
   },
-  //事件处理函数
-  bindViewTap: function() {
+  onLoad: function (options) {
+    // Save reference to page
+    let page = this;
+
+    // Display toast when loading
+    wx.showToast({
+      title: 'Updating',
+      icon: 'success',
+      duration: 3000
+    });
+
+    // Update local data
+    this.setData(app.globalData)
+  },
+  //References getApp() to load on the global data onto this page.
+
+  showRestaurant(e) {
+    const data = e.currentTarget.dataset;
+    const restaurant = data.restaurant;
+
     wx.navigateTo({
-      url: '../logs/logs'
-    })
+      url: `../show/show?id=${restaurant.id}`
+    });
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
